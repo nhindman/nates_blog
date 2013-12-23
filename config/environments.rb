38897d7b74a0668c5c897d94cf73aa1a788eafba
omardelarosa <thedelarosa@gmail.com>
2013-12-23 13:50:51 -0500

    made a few config changes

--- config/environments.rb
+++ config/environments.rb
@@ -3,21 +3,23 @@ configure :development do
   ActiveRecord::Base.establish_connection(
       :adapter => "postgresql",
       :host => "localhost",
-      :username => "nhindman",
-      :database => "simple_blog",
+      # :username => "nhindman",
+      # :database => "simple_blog",
+      :username => "omardelarosa",
+      :database => "simple_blog_nate",
       :encoding => "utf8"
     )
 end
 
-# configure :production do
-#   # HEROKU CONFIG 
-#   # db = URI.parse(ENV['HEROKU_POSTGRESQL_CYAN_URL '])
-#   #configuration info
-#   ActiveRecord::Base.establish_connection(
-#       :adapter => "postgresql",
-#       :host => "ec2-54-204-43-139.compute-1.amazonaws.com",
-#       :username => "xukfbnsaqtkguk",
-#       :database => "d2vn6f2aq0r347", 
-#       :password => "1DEnqhM8NM1-zFZfPSWdp8yuOh"
-#   )
-# end
+configure :production do
+  # HEROKU CONFIG 
+  # db = URI.parse(ENV['HEROKU_POSTGRESQL_CYAN_URL '])
+  #configuration info
+  ActiveRecord::Base.establish_connection(
+      :adapter => "postgresql",
+      :host => "ec2-54-204-43-139.compute-1.amazonaws.com",
+      :username => "xukfbnsaqtkguk",
+      :database => "d2vn6f2aq0r347", 
+      :password => "1DEnqhM8NM1-zFZfPSWdp8yuOh"
+  )
+end
